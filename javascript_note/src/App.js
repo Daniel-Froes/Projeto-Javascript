c873715cff1db50aa860d4797fa0175fca640620
Daniel Froes <danielfroestbf@gmail.com>
2022-05-09 23:18:45 -0300

    feat: adicionando react e uma interface grafica

---
+++ javascript_note/src/App.js
@@ -0,0 +1,14 @@
+import React,{ Fragment} from 'react';
+import './App.scss';
+import Header from './components/header';
+
+const App = () => (
+  <Fragment>
+    <Header/>
+  </Fragment>
+  
+    
+
+)
+
+export default App;
