7b0a98da31f7bb9fc0b1c23d73e3f306ac195d70
Daniel Froes <danielfroestbf@gmail.com>
2022-05-10 21:47:45 -0300

    feat: adicionando sistema de registro e login

---
+++ javascript_note/src/components/auth/login_form/index.js
@@ -0,0 +1,81 @@
+import React, { Fragment, useState } from 'react';
+import { Button, Field, Control, Input, Column, Section, Help, Label } from "rbx";
+import { Link, useNavigate } from "react-router-dom";
+import UserService from '../../../services/users'
+
+
+
+
+
+function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+    const navigate = useNavigate();
+
+
+
+
+ const HandleSubmit = async (evt) =>{
+    evt.preventDefault();
+
+    try{
+      const user = await UserService.login({ email: email, password: password})
+       navigate('/notes')
+    }catch(error){
+      setError(true);
+
+    }
+  }
+
+
+  return (
+    <Fragment>
+        <Column.Group centered>
+          <form onSubmit={HandleSubmit}>
+            <Column size={12}>
+              <Field>
+                <Label size="small">Email:</Label>
+                <Control>
+                  <Input
+                    type="email"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
+                    required
+                    name="email"
+                  />
+                </Control>
+              </Field>
+              <Field>
+                <Label size="small">Password:</Label>
+                <Control>
+                  <Input
+                    type="password"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
+                    required
+                    name="password"
+                  />
+                </Control>
+              </Field>
+              <Field>
+                <Control>
+                  <Column.Group>
+                    <Column>
+                      <Link to="/register" className="button is-white has-text-custom-purple">Register or</Link>
+                    </Column>
+                    <Column>
+                      <Button Link to="/notes" color="custom-purple" outlined>Login</Button>
+                    </Column>
+                  </Column.Group>
+                </Control>
+              </Field>
+              { error && <Help color="danger">Email or Password invalid</Help> }
+            </Column>
+          </form>
+        </Column.Group>
+    </Fragment>
+  )
+}
+
+export default LoginForm;
--- javascript_note/src/screens/auth/login/index.js
+++ javascript_note/src/screens/auth/login/index.js
@@ -1,40 +1,46 @@
 import React, { Fragment } from "react";
 import Header from "../../../components/header";
 import { Column, Section, Title, Container, Card } from "rbx";
-import LogoImage from "../../../assets/images/logo.png";
+import logoImage from "../../../assets/images/logo.png";
+import LoginForm from "../../../components/auth/login_form";
 import "../../../styles/auth.scss";
+
 
 const LoginScreen = () => (
   <Fragment>
-    <Header />
+    <Header>
+    </Header>
     <Section size="medium" className="auth">
-      <Container>
-        <Column.Group centered>
-          <Column size={3}>
-            <Card>
-              <Card.Content>
-                <Section>
-                  <Column.Group centered>
-                    <Column size={12}>
-                      <img src={LogoImage} alt="logo" />
-                    </Column>
-                  </Column.Group>
-                  <Column.Group>
-                    <Column size={12}>
-                      <Title
-                        size={6}
-                        className="has-text-grey has-text-centered">
-                        Your notes on the cloud
-                      </Title>
-                    </Column>
-                  </Column.Group>
-                </Section>
-              </Card.Content>
-            </Card>
-          </Column>
-        </Column.Group>
-      </Container>
-    </Section>
+        <Container>
+          <Column.Group centered>
+            <Column size={3}>
+              <Card>
+                <Card.Content>
+                  <Section>
+                    <Column.Group centered>
+                      <Column size={12}>
+                        <img src={logoImage} alt="logoImage" />
+                      </Column>
+                    </Column.Group>
+                    <Column.Group>
+                      <Column size={12}>
+                        <Title
+                          size={6}
+                          className="has-text-grey has-text-centered">
+                          Your notes on the cloud
+                        </Title>
+                        
+                      </Column>
+                    </Column.Group>
+                  </Section>
+                  <LoginForm/>
+                </Card.Content>
+              </Card>
+            </Column>
+          </Column.Group>
+        </Container>
+        
+      </Section>
   </Fragment>
 );
 
